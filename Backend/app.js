5b4ead4328f97ea082f7551176512bfbd8853386
bamkhatri <bamkhatri24@gmail.com>
2022-03-22 02:35:34 +0545

    user athentication is done

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,17 @@
 const { json } = require('body-parser')
 const express = require('express')
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use(morgan('combined'))
 
 //import product routes
 const product = require('./routes/productRoute')
+const user = require('./routes/userRoute')
 app.use('/api/v1', product)
+app.use('/api/v1', user)
 
 module.exports = app
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const productController = require('../controllers/productController')
+const { isAuthenticatedUser, authorizeRoles } = require('../Middleware/auth')
 
 const router = express.Router()
 
-router.post('/create', productController.createProduct)
+router.post(
+  '/create',
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  productController.createProduct
+)
 router.get('/read', productController.getAllProduct)
-router.put('/update/:id', productController.updateProduct)
-router.delete('/delete/:id', productController.deleteProduct)
+router.put(
+  '/update/:id',
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  productController.updateProduct
+)
+router.delete(
+  '/delete/:id',
+  isAuthenticatedUser,
+  authorizeRoles('admin'),
+  productController.deleteProduct
+)
 router.get('/read/:id', productController.getProductDetails)
 
 module.exports = router
