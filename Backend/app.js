60dca31930f525094c19d424c87c9c5fbfa97cd7
bamkhatri <bamkhatri24@gmail.com>
2022-08-24 01:12:05 +0545

    minor changes

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,10 @@ const cookieParser = require('cookie-parser')
 
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
+const dotenv = require('dotenv')
+
+//congig
+dotenv.config({ path: 'Backend/config/config.env' })
 
 const app = express()
 app.use(express.json())
@@ -16,8 +20,10 @@ app.use(fileUpload())
 const product = require('./routes/productRoute')
 const user = require('./routes/userRoute')
 const order = require('./routes/orderRoute')
+const payment = require('./routes/paymentRoute')
 app.use('/api/v1', product)
 app.use('/api/v1', user)
 app.use('/api/v1', order)
+app.use('/api/v1', payment)
 
 module.exports = app
